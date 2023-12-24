d05e50a974571ecd88cba7429fd80c6798a72f3f
XinAnhDung <thwrvetser58@gmail.com>
2023-12-24 21:38:17 +0700

    Upload code

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    open: false,
+    text: ' Merry ChirstMas and Happy New Year 2024!!! '
+  },
+  methods: {
+    click: function() {
+      this.open = !this.open;
+    }
+  },
+  watch:{
+    open: function() {
+      if (this.open == true) {
+          document.body.className = 'open';
+          }
+      else {
+        document.body.className = '';
+      }
+    }
+  }
+})
